b4d0bc840e2b1d23061f0d654986b110377b5c1b
sureshmcamv <sureshmcamv@gmail.com>
2021-09-17 10:19:40 +0530

    "HelloWorld Testing"

---
+++ springbootGit/src/main/java/com/example/springbootGit/SpringbootGitApplication.java
@@ -0,0 +1,15 @@
+package com.example.springbootGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootGitApplication.class, args);
+		System.out.println("helloWorld!...");
+	}
+
+}
